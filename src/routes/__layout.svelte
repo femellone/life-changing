2135b6d6eb6e2d4db876604bee365afaafe81e74
Fernando Mellone <fernandomellone2@gmail.com>
2022-05-24 00:28:27 -0400

    blabla

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -79,27 +79,15 @@
 		<div class="px-2 pt-2 pb-3 space-y-1">
 			<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
 			<a
-				href="#"
+				href="/"
 				class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-				aria-current="page">Dashboard</a
+				aria-current="page">Inicio</a
 			>
 
 			<a
-				href="#"
+				href="/personajes"
 				class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-				>Team</a
-			>
-
-			<a
-				href="#"
-				class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-				>Projects</a
-			>
-
-			<a
-				href="#"
-				class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-				>Calendar</a
+				>Personajes</a
 			>
 		</div>
 	</div>
