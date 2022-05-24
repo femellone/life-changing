ddd0a233fc0ae5caa98098e8c84fcc8001f817fd
Fernando Mellone <fernandomellone2@gmail.com>
2022-05-24 02:41:01 -0400

    implements google auth with email and password

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,39 @@
-<script>
-	const personajes = [];
+<script lang="ts">
+	import { signIn } from '$lib/firebase';
+
+	let email: string = '';
+	let password: string = '';
 </script>
 
 <form
-	on:submit={(e) => {
+	on:submit={async (e) => {
 		e.preventDefault();
+		const response = await signIn(email, password);
+		console.log(response);
 	}}
 >
-	<input type="text" name="name" placeholder="Name" />
-	<input type="text" name="email" placeholder="Email" required autocomplete="off" />
-	<input type="submit" value="Submit" />
+	<input
+		type="text"
+		class="border border-blue-600 px-4 py-2 rounded"
+		name="email"
+		placeholder="Email"
+		bind:value={email}
+		required
+		autocomplete="off"
+	/>
+	<input
+		type="password"
+		class="border border-blue-600 px-4 py-2 rounded"
+		name="password"
+		placeholder="Password"
+		autocomplete="off"
+		bind:value={password}
+	/>
+	<input
+		type="submit"
+		class="bg-blue-600 px-4 py-2 rounded text-white cursor-pointer"
+		value="Submit"
+	/>
 </form>
 
 <style>
